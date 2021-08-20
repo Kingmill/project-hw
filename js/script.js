2e7a61b47996336178fab8a35ef6f93777c700e1
kingmill <luto31072000@mail.ru>
2021-08-20 20:16:46 +1000

    Сделал задание
    1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-'use strict;'
+const fizzBuz=(numbegin,numend)=>
+{
+    for(let i = numbegin;i<=numend;i++){
+        if(i%3===0 && i%5===0){
+            console.log ('FizzBuz');
+        }
+         else if(i%3===0){
+           console.log ('Fizz');
+        }
+        else if(i%5===0){
+            console.log ('Buz');
+        }
+        else{
+            console.log (i);
+        }
+    }
+}
+console.log(fizzBuz(11,100));
+
+// type error
+// const num = 13;
+// const num1 = length(54);
+
+// reference error
+//aer(15);
+
+//syntax error
+// syntax=0;)
+// console.log(syntax);
